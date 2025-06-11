131abce42e6d7bf390dcb3a99cf1f7f4a1613b79
vinelis <lucavinelli4@gmail.com>
2025-06-11 11:23:32 -0300

    Primer paso al POM

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,18 +5,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.HomePage;
 
 import java.util.List;
 
+import static org.testng.Assert.assertEquals;
+
 public class BaseTests {
     private WebDriver driver;
+    protected HomePage homePage;
 
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver_vers137.0.7151.68.exe");
         driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/");
-        driver.manage().window().maximize();
 
+        driver.get("https://the-internet.herokuapp.com/");
+        homePage = new HomePage(driver);
+    }
+
+    @Test
+    public void testsCountMenus(){
         WebElement shiftingContent = driver.findElement(By.linkText("Shifting Content"));
         shiftingContent.click();
 
@@ -24,14 +36,13 @@ public class BaseTests {
         example1.click();
 
         List<WebElement> menus = driver.findElements(By.tagName("li"));
-        System.out.println(menus.size());
+        assertEquals(menus.size(), 5);
 
+    }
+
+    @AfterClass
+    public void tearDown(){
         driver.quit();
-
     }
 
-    public static void main(String args[]){
-        BaseTests test = new BaseTests();
-        test.setUp();
-    }
 }
